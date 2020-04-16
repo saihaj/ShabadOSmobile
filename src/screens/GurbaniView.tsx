20304d12ac4657dfb1cecf0eb9510477a1b6a1ba
saihaj <saihajpreet.singh@gmail.com>
2020-04-15 20:31:58 -0500

    build(screen): mockup shabad view
    Creates a shabad view for testing purposes. Renders data using stored JSON file.
    Related #49

---
+++ src/screens/GurbaniView.tsx
@@ -0,0 +1,71 @@
+import React, { FunctionComponent } from 'react'
+import { StyleSheet, SafeAreaView } from 'react-native'
+import { Container, View, Text, Content } from 'native-base'
+
+import NavigationBar from '../components/NavigationBar'
+
+import shabadData from '../mock/shabad.json'
+
+type GurbaniViewScreen = { };
+
+const styles = StyleSheet.create( {
+  title: {
+    backgroundColor: 'rgb(235,235,235)',
+    paddingTop: '6%',
+    paddingStart: '5%',
+    paddingEnd: '2%',
+    paddingBottom: '2%',
+  },
+  titleText: {
+    fontFamily: 'OpenGurbaniAkhar-Black',
+    fontSize: 35,
+    fontWeight: 'bold',
+  },
+  content: {
+    paddingTop: '2%',
+    paddingStart: '5%',
+    paddingEnd: '5%',
+  },
+  contentTextGurmukhi: {
+    fontSize: 30,
+  },
+  contentTextGurmukhiTranslation: {
+    fontSize: 15,
+  },
+  contentTextEnglishTranslation: {
+    paddingTop: '2%',
+    fontSize: 15,
+    paddingBottom: '5%',
+  },
+} )
+
+// console.log( shabadData.shabad[ 0 ].line.gurmukhi.unicode )
+const GurbaniViewScreen: FunctionComponent<GurbaniViewScreen> = () => (
+  <Container>
+    <SafeAreaView style={styles.title} />
+    <Content>
+      <View style={styles.title}>
+        <Text style={styles.titleText}>
+          imlu myry pRIqmw jIau quDu ibnu KrI inmwxI ]
+        </Text>
+      </View>
+      <View style={styles.content}>
+        {shabadData.shabad.map( ( lines ) => (
+          <View>
+            <Text style={styles.contentTextGurmukhi}>
+              {lines.line.gurmukhi.unicode}
+            </Text>
+            <Text style={styles.contentTextGurmukhiTranslation}>
+              {lines.line.translation.punjabi.default.unicode}
+            </Text>
+            <Text style={styles.contentTextEnglishTranslation}>
+              {lines.line.translation.english.default}
+            </Text>
+          </View>
+        ) )}
+      </View>
+    </Content>
+    <NavigationBar />
+  </Container>
+)
+export default GurbaniViewScreen
