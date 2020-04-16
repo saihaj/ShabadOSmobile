f17e52e9761f4fa494014a0b41b39b80309a6bd0
saihaj <saihajpreet.singh@gmail.com>
2020-04-15 20:32:51 -0500

    build(screen): landing page for the app
    Design home screen similar to mockup.

---
+++ src/screens/HomeScreen.tsx
@@ -0,0 +1,38 @@
+import React, { FunctionComponent } from 'react'
+import { Text, Container, Content } from 'native-base'
+import { StyleSheet, View, Image } from 'react-native'
+
+import NavigationBar from '../components/NavigationBar'
+
+import logo from '../../assets/logo.png'
+
+type HomeSreenProps = { }
+
+const styles = StyleSheet.create( {
+  welcomeContent: {
+    alignItems: 'center',
+  },
+  welcomeLogo: {
+    width: '30%',
+    height: 100,
+    marginTop: '5%',
+    marginBottom: '5%',
+  },
+} )
+
+const HomeScreen: FunctionComponent<HomeSreenProps> = () => (
+  <Container>
+    <Content>
+      <View style={styles.welcomeContent}>
+        <Image
+          style={styles.welcomeLogo}
+          source={logo}
+        />
+        <Text style={{ fontSize: 35, fontWeight: 'bold' }}>Shabad OS</Text>
+      </View>
+    </Content>
+    <NavigationBar />
+  </Container>
+)
+
+export default HomeScreen
